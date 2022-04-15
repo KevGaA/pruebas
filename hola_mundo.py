af37f7be19284d0cb57d4d5235d46aaa2f489a91
Kevin Gallardo <kevingallardo904@gmail.com>
2022-04-15 19:33:26 -0400

    1

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,18 +1,2 @@
 
 print("hola mundo que onda")
-<<<<<<< HEAD
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-print("hola mundo que onda")
-=======
->>>>>>> juancarlo
